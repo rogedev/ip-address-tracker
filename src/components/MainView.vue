2303b5f02e99fb4dd3be3f93d417c6b299d4cc56
Carlos Rogelio Rivas <apteruno@gmail.com>
2022-06-13 14:02:14 -0500

    fix: adress to address text

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -54,7 +54,7 @@ export default {
       ip: null,
       detail: {
         ip: {
-          title: 'IP ADRESS',
+          title: 'IP ADDRESS',
           value: '-'
         },
         location: {
